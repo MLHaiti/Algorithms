8c147816132dcfb480b03588e06b9d790a7eba04
Madsen Servius <smervius@gmail.com>
2019-03-29 20:44:32 -0500

    binary search tree - insert method

---
+++ binarySearchTree.py
@@ -0,0 +1,24 @@
+# Binary Search Trees
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+        else:
+            self.insertNode(data, self.root)
+
+    def insertNode(self, data, node):
+        if data < node.data:
+            if node.leftChild:
+                self.insertNode(data, node.leftChild)
+            else:
+                node.leftChild = Node(data)
