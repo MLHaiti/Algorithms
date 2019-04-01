59e5133f24b83a5001dda8a3739cfaa7ac9b8a63
Madsen Servius <smervius@gmail.com>
2019-04-01 07:26:51 -0500

    sms to send

--- datastructures/use_queue.py
+++ datastructures/use_queue.py
@@ -2,7 +2,7 @@ from queue import Queue
 
 # we can use queue for task scheduling
 # for example to send sms in the good order
-messageToSent = Queue()
+smsToSend = Queue()
 
 sms_1 = {
     'id': 1,
@@ -22,11 +22,11 @@ sms_3 = {
     'body': "Rappel de votre rendez-vous ce dimanche"
 }
 
-messageToSent.enqueue(sms_1)
-messageToSent.enqueue(sms_2)
-messageToSent.enqueue(sms_3)
+smsToSend.enqueue(sms_1)
+smsToSend.enqueue(sms_2)
+smsToSend.enqueue(sms_3)
 
-# sending the first message first
-print(f"sending ... : {messageToSent.dequeue()}")
-print(f"sending ... : {messageToSent.dequeue()}")
-print(f"sending ... : {messageToSent.dequeue()}")
+# sending the first message first written each time
+print(f"sending ... : {smsToSend.dequeue()}")
+print(f"sending ... : {smsToSend.dequeue()}")
+print(f"sending ... : {smsToSend.dequeue()}")
